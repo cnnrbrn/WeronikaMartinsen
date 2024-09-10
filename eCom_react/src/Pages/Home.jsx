e89301069f4fa9161dc673a55090010031460ccf
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-09-10 21:37:12 +0200

    Tailwind fails, adds configurations

---
+++ eCom_react/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import "../styles/index.css";
+
+function Home() {
+  return (
+    <>
+      <div className="bg-slate-600">
+        <h1 className="">Hey!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
