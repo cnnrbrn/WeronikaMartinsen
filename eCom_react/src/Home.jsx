d4c56cede1f303eeef5814462446e79187dca94c
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-09-10 21:22:42 +0200

    Adds files and configured and installs Tailwind

---
+++ eCom_react/src/Home.jsx
@@ -0,0 +1,13 @@
+import "./styles/index.css";
+
+function Home() {
+  return (
+    <>
+      <div className="">
+        <h1 className="">Tailwind CSS is Working!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
