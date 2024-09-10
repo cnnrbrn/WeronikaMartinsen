f023b1ecdcaa5677483141fa1dff53f8d604f3d1
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-09-10 21:50:15 +0200

    Tailwind test - tailwind starts

--- eCom_react/src/Pages/Home.jsx
+++ eCom_react/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import "../styles/index.css";
 function Home() {
   return (
     <>
-      <div className="bg-slate-600">
+      <div className="bg-slate-600 flex justify-center align-middle text-gray-50">
         <h1 className="">Hey!</h1>
       </div>
     </>
