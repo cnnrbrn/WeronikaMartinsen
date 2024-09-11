5e825b7fed624df9e2b7397433f5987275a64fdb
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-09-11 22:34:05 +0200

    Adds and styles header

---
+++ eCom_react/src/Layout/Header.jsx
@@ -0,0 +1,155 @@
+import { useState } from "react";
+import "../styles/index.css";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <>
+      <header className="bg-rose-50">
+        <nav
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex">
+            <div className="flex lg:flex-1">
+              <a href="/" className="-m-1.5 p-1.5">
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt="Logo"
+                />
+              </a>
+            </div>
+            {/* Mobile menu */}
+            {isMenuOpen && (
+              <div
+                className="sm:hidden mx-auto"
+                role="dialog"
+                aria-modal="true"
+              >
+                <div className="fixed inset-0 z-10 bg-rose-50"></div>
+                <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                  <div className="flex items-center justify-between">
+                    <a href="#" className="-m-1.5 p-1.5">
+                      <span className="sr-only">Your Company</span>
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Logo"
+                      />
+                    </a>
+                    <button
+                      type="button"
+                      onClick={toggleMenu}
+                      className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <svg
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                  <div className="mt-6 flow-root bg-rose-50">
+                    <div className="-my-6 divide-y divide-gray-500/10">
+                      <div className="space-y-2 py-6">
+                        <a
+                          href="/"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Home
+                        </a>
+                        <a
+                          href="/"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Contact
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="flex sm:hidden">
+            <button
+              type="button"
+              onClick={toggleMenu}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              aria-expanded={isMenuOpen ? "true" : "false"}
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                {isMenuOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+          <div className="hidden sm:flex sm:gap-x-12 bg-rose-50">
+            <div className="relative">{/* Dropdown menu */}</div>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Contact
+            </a>
+          </div>
+          <div className="lg:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
--- eCom_react/src/Pages/Home.jsx
+++ eCom_react/src/Pages/Home.jsx
@@ -1,11 +1,10 @@
 import "../styles/index.css";
+import Header from "../Layout/Header";
 
 function Home() {
   return (
     <>
-      <div className="bg-slate-600 flex justify-center align-middle text-gray-50">
-        <h1 className="">Hey!</h1>
-      </div>
+      <Header />
     </>
   );
 }
