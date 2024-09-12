47d599cb54826d60923f5baec769f78217b5a0b9
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-09-12 21:26:29 +0200

    Adds searchbar ui

--- eCom_react/src/Layout/Layout.jsx
+++ eCom_react/src/Layout/Layout.jsx
@@ -1,13 +1,16 @@
 import "../styles/index.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import SearchBar from "../Components/SearchBar";
 
 function Layout() {
   return (
     <>
       <div className="h-screen flex flex-col p-0 m-0">
         <Header />
-        <article className="h-screen"></article>
+        <main className="h-screen w-full bg-rose-50 flex justify-center">
+          <SearchBar />
+        </main>
         <Footer />
       </div>
     </>
